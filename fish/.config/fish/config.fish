b0dc0c47269ba33c7233da780689f840ca4b183e
elliotthilaire <elliott.hilaire@gmail.com>
2015-06-30 19:20:23 +1000

    use eggeggs rvm config
    it automatically loads rvm for a new terminal
    https://gist.github.com/eggegg/6077153

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,9 @@
 # enable autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and . /usr/local/share/autojump/autojump.fish
 
+# https://gist.github.com/eggegg/6077153
+# Load the default rubies
+if test -z $rvm_bin_path
+  exec bash --login -c "exec fish" ^&1
+end
+
