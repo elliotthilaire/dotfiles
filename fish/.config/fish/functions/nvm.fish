c30cbb4170cb4cde0faad4e6297625a6f7d2603d
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-11-20 14:54:47 +0000

    Stop using nvm

--- fish/.config/fish/functions/nvm.fish
+++
@@ -1,9 +0,0 @@
-# https://github.com/edc/bass/issues/9
-
-# This may cause a slow down in shell start time.
-# https://github.com/fish-shell/fish-shell/issues/2411
-# https://github.com/creationix/nvm/issues/703
-
-function nvm
-  bass source ~/.nvm/nvm.sh ';' nvm $argv
-end
