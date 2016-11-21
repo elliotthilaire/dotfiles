b23fa48ebee76c912bb73d1d1e3b49ecd2c21be6
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-11-21 20:41:22 +0000

    Add workaround for node executables not working when installed via yarn

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,9 @@ status --is-interactive; and . (rbenv init -|psub)
 
 # used by tmuxinator
 set --export EDITOR vim
+
+# work around to fix executable packages not working
+# when installed via yarn
+# https://github.com/yarnpkg/yarn/issues/1321
+# https://github.com/yarnpkg/yarn/issues/1389
+set --export PATH $PATH (yarn global bin)
